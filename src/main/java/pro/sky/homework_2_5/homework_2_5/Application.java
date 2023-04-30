6138bd83e8b7bc89bd26e1dac3068ad684931c8c
rom064 <rom064@mail.ru>
2023-04-30 23:04:13 +0500

    ДЗ 2.5

---
+++ src/main/java/pro/sky/homework_2_5/homework_2_5/Application.java
@@ -0,0 +1,13 @@
+package pro.sky.homework_2_5.homework_2_5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/homework_2_5/homework_2_5/ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.homework_2_5.homework_2_5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
