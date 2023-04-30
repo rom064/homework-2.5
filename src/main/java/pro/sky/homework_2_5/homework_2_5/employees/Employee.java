4a6c5482320a930e94be18f36f5d4dabfd4721aa
rom064 <rom064@mail.ru>
2023-05-01 01:40:23 +0500

    ДЗ 2.5 не получается((

--- src/main/java/pro/sky/homework_2_5/homework_2_5/employees/Employee.java
+++ src/main/java/pro/sky/homework_2_5/homework_2_5/employees/Employee.java
@@ -1,10 +1,32 @@
 package pro.sky.homework_2_5.homework_2_5.employees;
 
+
 import java.util.Objects;
 
 public class Employee {
-    private final String firstName;
-    private final String lastName;
+    private  String firstName;
+    private  String lastName;
+
+    public Employee(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
     @Override
     public boolean equals(Object o) {
@@ -17,19 +39,6 @@ public class Employee {
     @Override
     public int hashCode() {
         return Objects.hash(firstName, lastName);
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public Employee(String firstName, String lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
     }
 
     @Override
